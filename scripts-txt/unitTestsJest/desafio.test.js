cd50cfbdf33c2640d0fb9652fa02e8a82fb16192
ribYuriRethink <yuri.maycon@rethink.dev>
2023-03-21 14:06:40 -0300

    add unitTestsJest

---
+++ scripts-txt/unitTestsJest/desafio.test.js
@@ -0,0 +1,8 @@
+// const ds = require("./desafio");
+
+deescribe("doubleANumber", () => {
+  it("should return a double of the number", () => {
+    const result = doubleANumber(2);
+    expect(result).toBe(2);
+  });
+});
